440c6c7ffae209f2c6c8f38f0485390eb7d9364a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-09-28 16:53:52 +0100

    More checks of transitive p/q

--- tests/testthat/test-distcrete.R
+++ tests/testthat/test-distcrete.R
@@ -4,9 +4,13 @@ test_that("basic", {
   shape <- 3
   rate <- 1.15
 
-  d <- distcrete("gamma", 1, shape, rate, w = 0.5)
-  x <- seq(0, 20, by = d$interval)
+  for (w in c(0, 0.25, 0.5, 0.99)) {
+    d <- distcrete("gamma", 1, shape, rate, w = w)
+    x <- seq(0, 20, by = d$interval)
 
-  expect_equal(sum(d$d(x)), d$cdf(max(x) + 0.5), tolerance = 1e-14)
-  expect_equal(d$q(d$p(x)), x)
+    expect_equal(sum(d$d(x)), d$cdf(max(x) + 1 - w), tolerance = 1e-14)
+    px <- d$p(x)
+    expect_equal(d$q(px), x)
+    expect_equal(d$p(d$q(px)), px)
+  }
 })
